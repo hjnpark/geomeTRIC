20bdb7233c80f33710a3763389c222012de7dec4
Heejune Park <heepark@ucdavis.edu>
2023-08-23 12:47:21 -0700

    args can be passed by users for the interpolation method

--- geometric/interpolate.py
+++ geometric/interpolate.py
@@ -7,6 +7,7 @@ import json
 import numpy as np
 import networkx as nx
 import scipy.special
+from geometric.params import InterpParams, parse_interpolate_args
 from geometric.molecule import *
 from geometric.internal import *
 from geometric.nifty import ang2bohr, logger, commadash
@@ -1182,9 +1183,10 @@ class Interpolator(object):
                 print("Failed after 3 splits; exiting")
 
 def main():
-    M0 = Molecule(sys.argv[1])
-
-    interpolator = Interpolator(M0, align_system=True, do_prealign=False)
+    args = parse_interpolate_args(sys.argv[1:])
+    params = InterpParams(**args)
+    M0 = Molecule(args['input'])
+    interpolator = Interpolator(M0, n_frames=params.nframes, use_midframes=params.optimize, align_system=params.align, do_prealign=params.prealign)
     interpolator.run_workflow()
     
 if __name__ == "__main__":
