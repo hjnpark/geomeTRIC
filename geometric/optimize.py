98e4fb65af39cd617ea3a5ba35da72f2f4ce1b94
Lee-Ping Wang <leeping@ucdavis.edu>
2023-10-31 13:37:22 -0700

    mintrust can be lower than convergence_drms ; conmethod 1 bugfix

--- geometric/optimize.py
+++ geometric/optimize.py
@@ -818,6 +818,8 @@ class Optimizer(object):
             else:
                 self.SortedEigenvalues(self.H)
                 logger.info("Converged! =D\n")
+                if self.trust < min(1.2e-3, params.Convergence_drms):
+                    logger.info("*_* Warning - trust radius is lower than RMS displacement convergence criterion; please check gradient accuracy. *_*\n")
                 self.state = OPT_STATE.CONVERGED
             return True, step_state
 
